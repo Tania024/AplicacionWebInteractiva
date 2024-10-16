0b76fb35ca7d5989878c515d4addaba1b3852b2b
Tania024 <84852408+Tania024@users.noreply.github.com>
2024-10-15 23:01:54 -0500

    Contacto y pie de pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent],
+  imports: [RouterOutlet, InicioComponent, ContactoComponent, PiePaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
