592ee84ded99cdec065650d62e6ccbb274429ce3
Tania024 <84852408+Tania024@users.noreply.github.com>
2024-10-16 08:42:10 -0500

    Separado por secciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent, ContactoComponent, PiePaginaComponent],
+  imports: [RouterOutlet,InicioComponent,  PiePaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { AgregarCursoComponent } from './components/agregar-curso/agregar-curso.component';
+import { CursoComponent } from './components/curso/curso.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "Inicio", component:InicioComponent},
+    {path:"Curso", component:CursoComponent},
+    {path:"AgregarCursos", component:AgregarCursoComponent},
+    {path:"Contacto", component:ContactoComponent}
+];
